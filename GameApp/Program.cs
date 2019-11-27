a1b2f158e4b20553d3edde97f8f5ac0f5b35d8e3
German <german.nikolishin@gmail.com>
2019-11-27 17:23:01 +0000

    add basic files

--- GameApp/Program.cs
+++ GameApp/Program.cs
@@ -18,9 +18,6 @@ namespace GameApp
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
