84647d3ab3535df4be1276204b369d8d45c9814e
German <german.nikolishin@gmail.com>
2020-01-19 15:00:27 +0000

    basic logging

--- GameService.API/Controllers/MainController.cs
+++ GameService.API/Controllers/MainController.cs
@@ -8,6 +8,7 @@ using GameService.Domain.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using Serilog;
 
 namespace GameApp.Controllers
 {
@@ -26,6 +27,7 @@ namespace GameApp.Controllers
         // GET
         public IActionResult Index()
         {
+            Log.Information("Loading index");
             return Ok(
                 _mongoRepository
                     .GetListSorted(x => true, 
@@ -37,6 +39,7 @@ namespace GameApp.Controllers
         [HttpGet("list/{page:int}")]
         public IActionResult GetList(int page = 1)
         {
+            Log.Information($"Requesting list #{page}");
             if (page < 1) return BadRequest();
             var list = _mongoRepository
                 .GetListSorted(x => true,
@@ -56,15 +59,17 @@ namespace GameApp.Controllers
 
             try
             {
+                Log.Information($"Attempt to create game with code {game.Code}");
                 _mongoRepository.CreateOne(game);
             }
             catch (MongoWriteException e)
             {
+                Log.Warning(e,$"Error occured while creating new game {game.Code}");
                 return BadRequest(e);
             }
             
             //TODO: set messages to frontend to start
-            
+            Log.Information($"The game {game.Code} has been created successfully");
             return Ok();
         }
 
@@ -72,11 +77,20 @@ namespace GameApp.Controllers
         [HttpPost("user_joined")]
         public IActionResult UserJoined([FromBody] UserJoinedDTO info)
         {
+            Log.Information("Attempt to register new player");
             var currentGame = _mongoRepository.GetOne(info.GameCode);
-            if (currentGame == null) return BadRequest();
+            if (currentGame == null)
+            {
+                Log.Warning($"The game {info.GameCode} does not exist");
+                return BadRequest();
+            }
 
             var currentTeam = currentGame.Teams.FirstOrDefault(x => string.Equals(x.Code, info.SchoolCode));
-            if (currentTeam == null) return BadRequest();
+            if (currentTeam == null)
+            {
+                Log.Warning($"The team {info.SchoolCode} does not exist");
+                return BadRequest();
+            }
 
             currentTeam.NumberOfPlayers++;
 
@@ -88,6 +102,7 @@ namespace GameApp.Controllers
             //TODO: recalculate constant
 
             _mongoRepository.Update(filter, updateDefinition);
+            Log.Information($"New player has successfully joined {currentTeam.Name}");
 
             return Ok();
         }
